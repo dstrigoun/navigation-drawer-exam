97eefda156d3f6b8d32000470522e048b188599b
Dasha Strigoun <dstrigoun@outlook.com>
2018-06-13 17:27:42 -0700

    Add files via upload

---
+++ app/src/main/java/ca/bcit/cst/comp3617midterm/MainActivity.java
@@ -0,0 +1,69 @@
+package ca.bcit.cst.comp3617midterm;
+
+import android.graphics.Color;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+    private DrawerLayout drawerLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        int color;
+                        drawerLayout.closeDrawers();
+
+                        switch (item.getItemId()) {
+                            case R.id.nav_first:
+                                color = Color.RED;
+                                break;
+                            case R.id.nav_second:
+                                color = Color.BLUE;
+                                break;
+                            case R.id.nav_third:
+                                color = Color.GREEN;
+                                break;
+                            default:
+                                color = Color.WHITE;
+                        }
+
+                        getWindow().getDecorView().setBackgroundColor(color);
+
+                        return true;
+                    }
+                }
+        );
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
